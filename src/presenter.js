b172f101dc4d80182c013fbf5a554ec8ec3b0e5b
Arturo <arturo.veliz@ucb.edu.bo>
2024-02-20 00:17:37 -0400

    Saludador por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,10 @@
 const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
+const nombre=document.querySelector("#nombre");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const nombreS=String(nombre.value);
   const saludo='Hola';
-  div.innerHTML = "<p><b>"+saludo +"</b></p>";
+  div.innerHTML = "<p>"+saludo +"<b> " + nombreS + "</b></p>";
 });
